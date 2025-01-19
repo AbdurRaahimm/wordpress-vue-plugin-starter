455e93d9f5015ef7374d38c70ebc528d68d02d05
AbdurRaahimm <19203203015@cse.bubt.edu.bd>
2025-01-19 23:31:40 +0600

    add Message component and refactor App.vue to use it

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <script setup>
-import { ref } from 'vue';
-
-    const message = ref("Hello from Vue 3 with a .vue file!");
-
-    const updateMessage = () => {
-        message.value = "You clicked the button!";
-    };
+import Message from './components/Message.vue';
 
 </script>
 
 <template>
-  <div>
-    <h1>{{ message }}</h1>
-    <button @click="updateMessage">Click Me</button>
-  </div>
+  <Message />
 </template>
-
-<style>
-h1 {
-  font-family: Arial, sans-serif;
-  color: #42b983;
-}
-button {
-  margin-top: 10px;
-  padding: 10px 20px;
-  font-size: 16px;
-}
-</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+    const message = ref("Sayed Abdur Rahim");
+
+    const updateMessage = () => {
+        message.value = "You clicked the button!";
+    };
+
+</script>
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="updateMessage">Click Me</button>
+  </div>
+</template>
+<style scope>
+h1 {
+  font-family: Arial, sans-serif;
+  color: #42b983;
+}
+button {
+  margin-top: 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
